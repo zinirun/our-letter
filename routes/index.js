3cb773888c80a43a55700177a8d7bf8c2fc49e6d
zinirun <heojj97@naver.com>
2020-07-02 18:57:09 +0900

    Update letter_count at index

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,27 @@
-var ejs = require('ejs'),
+const ejs = require('ejs'),
     fs = require('fs');
+const mySqlClient = mysql.createConnection(require('../config/db_config'));
 
 function index(req, res) {
-    fs.readFile('./public/index.html', 'utf8', function (error, data) {
-        res.send(ejs.render(data, {}));
+
+    const countLetterSql = 'select count(*) as cnt from letter;';
+
+    mySqlClient.query(countLetterSql, params, function (err) {
+        if (err) {
+            console.log("Insert Error>>" + err);
+            fs.readFile('./public/index.html', 'utf8', function (error, data) {
+                res.send(ejs.render(data, {
+                    letter_count = 'ERROR'
+                }));
+            });
+        } else {
+            fs.readFile('./public/index.html', 'utf8', function (error, data) {
+                res.send(ejs.render(data, {
+                    letter_count = rows[0].cnt
+                }));
+            });
+        }
     });
 }
+
 module.exports = index;
