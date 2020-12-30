43142e43dfa7f98854e6297ec01f7dfa8ed35f5f
zinirun <heojj97@naver.com>
2020-12-30 17:51:26 +0900

    update configs

--- config/db_config.js
+++ config/db_config.js
@@ -1,22 +1,22 @@
 //FOR SERVER
 module.exports = {
-    connectionLimit: 30,
-    host: process.env.DATABASE_HOST,
-    user: 'root',
-    password: 'sw32164959@',
-    database: 'ourletter_db',
-    dateStrings: 'date',
-    charset  : 'utf8',
-    debug: false,
-    insecureAuth: true
-}
+  connectionLimit: 30,
+  host: process.env.DATABASE_HOST,
+  user: "root",
+  password: "",
+  database: "ourletter_db",
+  dateStrings: "date",
+  charset: "utf8",
+  debug: false,
+  insecureAuth: true,
+};
 //
 ////FOR LOCAL TEST
 //module.exports = {
 //    connectionLimit: 20,
 //    host: 'localhost',
 //    user: 'root',
-//    password: 'wjswls1',
+//    password: '',
 //    database: 'ourletter_db',
 //    dateStrings: 'date',
 //    debug: false
